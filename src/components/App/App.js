f926b64709dc787ba0b1cd9c182b2fff403e5108
Ruslan Koroteev <menu85@yandex.ru>
2018-09-25 17:28:07 +0200

    ready

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import React from 'react';
+
+class Mycomponent extends Component {
+  render() {
+    return( <div className='App'>
+              <p className='description'>Миру-мир, студентам - beer.</p> 
+            </div>
+    )}
+}
+
+// const Mycomponent = ()=> {
+
+//   return (
+//     <div className='App'>
+//       <p className='description'>Миру-мир, студентам - beer.</p> 
+//     </div>   
+//   )
+// }
+
+export default Mycomponent;
