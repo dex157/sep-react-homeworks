320568e1b2c56859561b2c092649e69d3859a253
Sergey Tikhomirov <32257840+TikhomirovSergei@users.noreply.github.com>
2018-09-25 10:37:11 +0300

    Adding a file 'integration/emptyTest.js'
    1) Adding a file 'integration/emptyTest.js' for a successful command 'yarn run cypress:run';
    2) Add an indent for readability.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default class App extends React.Component<{}> {
+export default class App extends React.Component {
   render() {
     return (
       <div className='App'>
-       <p className='description'>
+        <p className='description'>
           Миру-мир, студентам - beer.
         </p>
       </div>
