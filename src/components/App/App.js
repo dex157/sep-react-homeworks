778998454164251152e565e32946979e1c650633
Ruslan Koroteev <menu85@yandex.ru>
2018-09-26 12:13:11 +0200

    clean code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-//import React from 'react';
 
 class Mycomponent extends Component {
   render() {
-    return( <div className='App'>
-              <p className='description'>Миру-мир, студентам - beer.</p> 
-            </div>
-    )}
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
 }
-
-// const Mycomponent = ()=> {
-
-//   return (
-//     <div className='App'>
-//       <p className='description'>Миру-мир, студентам - beer.</p> 
-//     </div>   
-//   )
-// }
 
 export default Mycomponent;
