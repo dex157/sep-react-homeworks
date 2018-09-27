097c865f40e43dbe577e6ad31b28200e78e0feb4
log1k <log1k@mail.ru>
2018-09-27 19:01:17 +0300

    dz2

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Message from '../Message/Message';
+import './Chat.css';
+
+class Chat extends React.Component {
+  state = {
+    messages: [],
+    messageInput: ''
+  };
+  changeInputMessage = e => {
+    this.setState({ messageInput: e.target.value });
+  };
+  sendMessageOnEnter = e => {
+    if (e.key === 'Enter') {
+      this.setState({
+        messages: [...this.state.messages, { text: this.state.messageInput }],
+        messageInput: ''
+      });
+    }
+  };
+  render() {
+    const { messages } = this.state;
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {messages.map(message => (
+              <Message key={message.text} text={message.text} />
+            ))}
+          </div>
+        </div>
+
+        <input
+          type="text"
+          value={this.state.messageInput}
+          onKeyPress={this.sendMessageOnEnter}
+          onChange={this.changeInputMessage}
+          className="input-message"
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
