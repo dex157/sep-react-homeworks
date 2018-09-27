d986dfd7cdf497a8987f6d37636db793cd5c89ec
Sergey Tikhomirov <32257840+TikhomirovSergei@users.noreply.github.com>
2018-09-27 11:38:47 +0300

    homework2

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,46 @@
-export { default } from './Chat';
+import React from 'react'
+import Message from '../Message'
+
+class Chat extends React.Component {
+    state = {
+        messages: [],
+        messageInput: ''
+    }
+
+    changeInputMessage = (event) => {
+        this.setState({ messageInput: event.target.value });
+    }
+
+    sendMessageOnEnter = (event) => {
+        const { messages, messageInput } = this.state;
+
+        if (event.key === 'Enter') {
+            this.setState({
+                messages: [...messages, { text: messageInput, id: messages.length }],
+                messageInput: ''
+            });
+        }
+    }
+
+    render() {
+        const { messages, messageInput } = this.state;
+
+        console.log(this.state);
+        return (
+            <div className='chat'>
+                <div className='message-list' >
+                    {messages.map((el, id) => (
+                        <div key={id} className='messages'>
+                            <Message text={el.text} />
+                        </div>
+                    ))}
+                </div>
+                <input className='input-message' value={messageInput}
+                    onChange={this.changeInputMessage}
+                    onKeyPress={this.sendMessageOnEnter} />
+            </div>
+        )
+    }
+}
+
+export default Chat;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,13 @@
-export { default } from './Message';
+import React from 'react'
+
+class Message extends React.Component {
+    render() {
+        return (
+            <span className="message">
+                {this.props.text}
+            </span>
+        )
+    }
+}
+
+export default Message;
