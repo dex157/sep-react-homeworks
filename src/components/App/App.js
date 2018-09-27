356033a5e9e56a0d274b366841b4f9d48617bfe8
Sergey Kozlov <sergey.kozlov@lindenvalley.de>
2018-09-27 23:40:17 +0400

    test complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,14 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-export default () => <p>Homework app</p>
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p className="description">Миру-мир, студентам - beer.</p>
+            </div>
+        );
+    }
+
+}
+
+export default App;
