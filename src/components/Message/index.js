4d5974da28d5ddfc894985aa6868bae0555e167f
Mariya Molchanova <molchanova.m2903@gmail.com>
2018-09-29 19:24:40 +0300

    Изменения в структуре проекта

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,14 +1,1 @@
-import React, { Component } from 'react';
-import './Message.css';
-
-class Message extends Component {
-  render() {
-    return (
-      <span className="message">
-        {this.props.text}
-      </span>
-    );
-  }
-}
-
-export default Message;
+export { default } from './Message'
