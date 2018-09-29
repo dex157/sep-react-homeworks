4103680bf609f5a4a85ab47343f2879899949322
Mariya Molchanova <molchanova.m2903@gmail.com>
2018-09-29 16:50:33 +0300

    Выполнено ДЗ2

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,14 @@
-export { default } from './Message';
+import React, { Component } from 'react';
+import './Message.css';
+
+class Message extends Component {
+  render() {
+    return (
+      <div className="message">
+        {this.props.text}
+      </div>
+    )
+  }
+}
+
+export default Message;
