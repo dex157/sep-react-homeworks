422a2bbe9680ef323954e56898972ff0531463dc
Artem Samofalov <dex157@gmail.com>
2018-09-30 19:56:39 +0300

    add homework

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import { AuthConsumer } from '../../contexts/Auth';
+import './Footer.css';
+
+class Footer extends PureComponent {
+  render() {
+    return 'empty';
+  }
+}
+
+export default Footer;
