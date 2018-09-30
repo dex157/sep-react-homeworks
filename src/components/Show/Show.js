eaecdec91a163c0bf4caf48361a76661b4c9a7fe
Sergey Kulakovskiy <kulakovskiy.s.v@gmail.com>
2018-09-30 21:43:17 +0300

    homework-tv-maze-show-key done

---
+++ src/components/Show/Show.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {getShowInfo} from '../../api';
+import './Show.css';
+
+
+class Show extends Component {
+    state = {
+        showId: '',
+        data: ''
+    }
+
+
+    componentDidMount() {
+
+        const {showId} = this.props;
+
+        if(showId !== '') {
+            getShowInfo(showId)
+            .then((data) => {
+                this.setState({
+                    showId: showId,
+                    data: data
+                })
+            });
+        }
+    }
+
+    render() {
+        let {data} = this.state;
+        
+        if(data) {
+            return (
+                <div className="show">
+                    <img className="show-image" src={data.image.original} alt={data.name}/>
+                    <h2 className="show-label t-show-name">{data.name}</h2>
+                    <p className="show-text t-show-genre"><b>Жанр: </b>{data.genres.join(', ')}</p>
+                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={{ __html: `${data.summary}` }}></p>
+                </div>
+            )
+        } else {
+            return <p className="show-inforation t-show-info">Шоу не выбрано</p>
+        }
+    }
+}
+
+export default Show;
