6ab8ccd382e4ef3dc18cb65475abf8b045a23bea
Artem Kostylev <kostylev95@yandex.ru>
2018-09-30 10:51:11 +0300

    create Show component

---
+++ src/components/Show/Show.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { getShowInfo } from '../../api';
+
+import './Show.css';
+
+export default class Show extends Component {
+  state = {
+    data: null,
+    showId: ''
+  };
+
+  componentDidUpdate() {
+    const props = this.props;
+    const state = this.state;
+
+    if (props.showId !== state.showId) {
+      this.setState({ showId: props.showId, data: null });
+    }
+
+    if (state.data === null && state.showId !== '') {
+      getShowInfo(state.showId).then(data => {
+        this.setState({ data: data });
+      });
+    }
+  }
+
+  renderShow = ({ image, name, genres, summary }) => (
+    <div className="show">
+      <img className="show-image" src={image.original} alt={name} />
+      <h2 className="show-label t-show-name">{name}</h2>
+      <p className="show-text t-show-genre">
+        <b>Жанр: </b>
+        {genres.join(', ')}
+      </p>
+      <p
+        className="show-text t-show-summary"
+        dangerouslySetInnerHTML={{ __html: summary }}
+      />
+    </div>
+  );
+
+  render() {
+    const { showId, data } = this.state;
+
+    if (showId === '') {
+      return <p className="show-inforation t-show-info">Шоу не выбрано</p>;
+    }
+
+    if (showId !== '' && data === null) {
+      return (
+        <p className="show-inforation t-show-info">
+          Загрузка шоу с id {showId}
+        </p>
+      );
+    }
+
+    return this.renderShow(data);
+  }
+}
