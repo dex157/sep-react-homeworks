95f49e9e6793f118d7031c75b4b2caac475fe4c9
Slav <shutrow@rambler.ru>
2018-10-01 23:27:14 +0300

    preparing environment

---
+++ src/components/Form/Form.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import './Form.css';
+
+export default class Form extends Component {
+    // инициализируем state
+    state = {
+        messages: [],
+        messageInput: ''
+    };
+
+    // обеспечиваем видимость элементов в окне формы
+    componentDidUpdate() {
+        this.scrollVisible();
+      }    
+  
+      // отслеживание изменений поля ввода
+      changeInputMessage = event => {
+          this.setState({
+              messageInput: event.target.value
+          });
+      };
+  
+      // добавление сообщения при нажатии Enter
+      sendMessageOnEnter = event => {
+        const { messages, messageInput } = this.state;
+        
+          if (event.key === 'Enter') {
+  
+              // добавляем запись в список messages, в конец списка
+              this.setState({
+                messages: [...messages, { text: messageInput }],
+                messageInput: ''
+              });
+  
+          } else {
+              return false;
+          }
+      };
+  
+      scrollVisible() {
+        if (this.elem) {
+          this.elem.scrollIntoView({ block: 'end', behavior: 'smooth' });
+        }
+      }
+  
+      render() {
+          return (
+              <div className="chat">
+                  <div className="message-list">
+                      <div className="messages"
+                        ref={elem => {
+                          this.elem = elem;
+                        }}
+                      >
+                          {this.state.messages.map((message, index) => {
+                              return <Message key={index} text={message.text}/>;
+                          })}
+                      </div>
+                  </div>
+  
+                  <input
+                      className="input-message"
+                      value={this.state.messageInput}
+                      onChange={this.changeInputMessage}
+                      onKeyPress={this.sendMessageOnEnter}
+                  />
+              </div>
+  
+          );
+      }
+  }    
