8ba76987a589af0e2cf90720fa7b3057f7c4b841
Sergey Kozlov <sergey.kozlov@lindenvalley.de>
2018-10-02 00:18:33 +0400

    homework-2

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,10 @@
-export { default } from './Message';
+import React, {Component} from 'react';
+import './Message.css';
+
+export default class Message extends Component {
+    render() {
+        return (
+            <span className="message">{this.props.text}</span>
+        )
+    }
+}
