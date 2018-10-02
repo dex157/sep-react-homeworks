92839dc66e0ab9dc6e0b3cbe4354fde22b7354de
Ruslan Koroteev <menu85@yandex.ru>
2018-10-02 23:31:18 +0200

    ready vs key

---
+++ src/components/Show/Show.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import './Show.css';
+import { getShowInfo } from '../../api.js';
+
+class Show extends PureComponent {
+  state = {
+    showId: '',
+    data: null
+  };
+
+  componentDidMount() {
+    const { showId } = this.props;
+
+    if (showId !== '') {
+      getShowInfo(showId).then(data => {
+        this.setState({
+          showId: showId,
+          data: data
+        });
+      });
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+    if (data) {
+      return (
+        <div className="show">
+          <img
+            className="show-image"
+            src={data.image.original}
+            alt={data.name}
+          />
+          <h2 className="show-label t-show-name">{data.name}</h2>
+          <p className="show-text t-show-genre">
+            <b>Жанр: </b>
+            {data.genres.join(', ')}
+          </p>
+          <p
+            className="show-text t-show-summary"
+            dangerouslySetInnerHTML={{ __html: `${data.summary}` }}
+          />
+        </div>
+      );
+    } else {
+      return <p className="show-inforation t-show-info">Шоу не выбрано</p>;
+    }
+  }
+}
+
+export default Show;
