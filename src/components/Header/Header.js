8d8b1485c47fa75e9563d3b6d00d85bbffcb4c0f
dmitry <pisarev_dmitry@mail.ru>
2018-10-02 18:48:45 +0300

    homework complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React, { PureComponent } from 'react';
 import { AuthConsumer } from '../../contexts/Auth';
-import Button from '../Button'
+import Button from '../Button';
 import './Header.css';
 
 class Header extends PureComponent {
   render() {
-    return ('empty');
+    return (
+      <AuthConsumer>
+        {({ isAuthorized, email, logout }) =>
+          isAuthorized ? (
+            <div className="header__content">
+              <div className="header-menu">
+                <p className="header-menu__email t-header-email">{email}</p>
+                <Button className="t-logout" onClick={logout}>
+                  Выйти
+                </Button>
+              </div>
+            </div>
+          ) : null
+        }
+      </AuthConsumer>
+    );
   }
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,46 @@
 import React, { Fragment, PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
 import './Layout.css';
+import composeClassNames from '../../utils/composeClassNames';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+    const { header, footer, children } = this.props;
+
+    return (
+      <Fragment>
+        {this.renderHeader(header)}
+        <main
+          className={composeClassNames(
+            'main',
+            header && 'main--with-header',
+            footer && 'main--with-footer'
+          )}
+        >
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+        {this.renderFooter(footer)}
+      </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    return HeaderChild ? (
+      <header className="header">
+        <SectionTitle className="header__title">Header</SectionTitle>
+        <HeaderChild />
+      </header>
+    ) : null;
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return FooterChild ? (
+      <footer className="footer">
+        <SectionTitle className="footer__title">Footer</SectionTitle>
+        <FooterChild />
+      </footer>
+    ) : null;
   }
 }
 
