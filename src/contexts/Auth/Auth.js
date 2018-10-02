d2b04e61d27bb0f41174c6bdb24c3aebbe8a674e
Sergey Kulakovskiy <kulakovskiy.s.v@gmail.com>
2018-10-02 13:49:09 +0300

    Auth file work

--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,51 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state={
+    isAuthorized: false,
+    email: '',
+    password: '',
+    authorizeError: '',
+  }
+
+  authorize = (email, password) => {
+    if(email === 'stu@dent.com' && password === 123){
+      this.setState({
+        isAuthorized: true,
+        email: '',
+        password: '',
+        authorizeError: ''
+      })
+    } else {
+      this.setState({
+        isAuthorized: false,
+        email: email,
+        password: password,
+        authorizeError: 'Email или пароль введён не верно'
+      })
+    }
+  }
+
+  logout = () => {
+    this.setState({
+      isAuthorized: false,
+      email: '',
+      password: '',
+      authorizeError: ''
+    })
+  }
+
+  getProviderValue = () => {
+
+  }
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    const { isAuthorized } = this.state;
+    return (
+      // <Provider value={{isAuthorized, authorize: this.authorize}}>{children}</Provider>
+      <Provider>{children}</Provider>
+      );
   }
 }
 
