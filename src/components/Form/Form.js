9d2838a564229091003b0b87ec9c585aa6767176
Olga Dudarieva <o.stuzzy@gmail.com>
2018-10-03 13:51:07 +0300

    homework form-validation done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,41 +1,99 @@
 import React from 'react';
 import './Form.css';
+import isLoginPic from './assets/bond_approve.jpg';
 
 class Form extends React.Component {
   state = {
-    name: '',
-    nameValid: false,
-    nameErMessage: ''
+    firstname: '',
+    firstnameErMessage: '',
+    lastname: '',
+    lastnameErMessage: '',
+    password: '',
+    passwordErMessage: '',
+    isLoggedIn: false
   };
 
-  validateName = name => {
-    if (name.length > 0) {
-      if (name !== 'James') {
-        this.setState({ nameErMessage: 'Имя указано не верно' });
+  //Validation
+  validateFirstName = value => {
+    if (value.length > 0) {
+      if (value !== 'James' && value !== 'james') {
+        this.setState({ firstnameErMessage: 'Имя указано не верно' });
+        return false;
       } else {
-        this.setState({ nameErMessage: '' });
+        this.setState({ firstnameErMessage: '' });
+        return true;
       }
     } else {
-      this.setState({ nameErMessage: 'Нужно указать имя' });
+      this.setState({ firstnameErMessage: 'Нужно указать имя' });
+      return false;
     }
   };
 
-  onChangeName = event => {
+  validateLastName = value => {
+    if (value.length > 0) {
+      if (value !== 'Bond' && value !== 'bond') {
+        this.setState({ lastnameErMessage: 'Фамилия указана не верно' });
+        return false;
+      } else {
+        this.setState({ lastnameErMessage: '' });
+        return true;
+      }
+    } else {
+      this.setState({ lastnameErMessage: 'Нужно указать фамилию' });
+      return false;
+    }
+  };
+
+  validatePassword = value => {
+    if (value.length > 0) {
+      if (value !== '007') {
+        this.setState({ passwordErMessage: 'Пароль указан не верно' });
+        return false;
+      } else {
+        this.setState({ passwordErMessage: '' });
+        return true;
+      }
+    } else {
+      this.setState({ passwordErMessage: 'Нужно указать пароль' });
+      return false;
+    }
+  };
+
+  onChangeField = event => {
     var val = event.target.value;
-    this.setState({ name: val });
-    this.setState({ nameErMessage: '' });
+    var label = event.target.name;
+    this.setState({
+      [label]: val,
+      firstnameErMessage: '',
+      lastnameErMessage: '',
+      passwordErMessage: ''
+    });
   };
 
   handSubmit = event => {
     event.preventDefault();
-    this.validateName(this.state.name);
+    var firstNameIsvalidate = this.validateFirstName(this.state.firstname);
+    var lastNameIsvalidate = this.validateLastName(this.state.lastname);
+    var passwordIsvalidate = this.validatePassword(this.state.password);
+    if (firstNameIsvalidate && lastNameIsvalidate && passwordIsvalidate) {
+      console.log('norm');
+      this.setState({ isLoggedIn: true });
+    }
   };
 
   render() {
-    return (
-      <div className="app-container">
+    let content;
+
+    if (this.state.isLoggedIn) {
+      content = (
+        <img src={isLoginPic} alt="bond approve" class="t-bond-image" />
+      );
+    } else {
+      content = (
         <form className="form" onSubmit={this.handSubmit}>
           <h1>Введите свои данные, агент</h1>
+
+          {/* Name field */}
           <p className="field">
             <label className="field__label" for="firstname">
               <span className="field-label">Имя</span>
@@ -45,12 +103,48 @@ class Form extends React.Component {
               type="text"
               name="firstname"
               value={this.state.name}
-              onChange={this.onChangeName}
+              onChange={this.onChangeField}
             />
             <span className="field__error field-error t-error-firstname">
-              {this.state.nameErMessage}
+              {this.state.firstnameErMessage}
             </span>
           </p>
+
+          {/* Lastname field */}
+          <p className="field">
+            <label className="field__label" for="lastname">
+              <span className="field-label">Фамилия</span>
+            </label>
+            <input
+              className="field__input field-input t-input-lastname"
+              type="text"
+              name="lastname"
+              value={this.state.lastname}
+              onChange={this.onChangeField}
+            />
+            <span className="field__error field-error t-error-lastname">
+              {this.state.lastnameErMessage}
+            </span>
+          </p>
+
+          {/* Password field */}
+          <p className="field">
+            <label className="field__label" for="password">
+              <span className="field-label">Пароль</span>
+            </label>
+            <input
+              className="field__input field-input t-input-password"
+              type="text"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChangeField}
+            />
+            <span className="field__error field-error t-error-password">
+              {this.state.passwordErMessage}
+            </span>
+          </p>
+
+          {/* Submit button */}
           <div className="form__buttons">
             <input
               type="submit"
@@ -59,8 +153,10 @@ class Form extends React.Component {
             />
           </div>
         </form>
-      </div>
-    );
+      );
+    }
+
+    return <div className="app-container">{content}</div>;
   }
 }
 
