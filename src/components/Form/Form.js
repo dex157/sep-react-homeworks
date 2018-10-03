3a73c490b3f8be0123597d96dac08960d002fff2
Slav <shutrow@rambler.ru>
2018-10-03 22:10:48 +0300

    fighting with reloads

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,71 +1,116 @@
 import React, {Component} from 'react';
 import './Form.css';
+import Bond from './assets/bond_approve.jpg';
 
 export default class Form extends Component {
-    // инициализируем state
-    state = {
-        messages: [],
-        messageInput: ''
+
+    // инициализируем поля пустыми значениями
+    // значения ошибок будем хранить в formErrors, первоначально значение ошибки - пустая строка
+    constructor (props) {
+        super(props);
+        this.state = {
+            values: {
+                firstname: 'Slav',
+                lastname: '',
+                password: '',
+            },    
+            errors: {
+                  firstname: '',
+                  lastname: '',
+                  password: ''
+            },
+            isSubmitted: false
+        };        
+    }
+
+    // обновляем содержимое state при изменении поля Input
+    handleUserInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value}, 
+            () => { console.log('input'); });
+    }
+
+    // отслеживание изменений поля ввода
+    changeInputMessage = event => {
+        this.setState({
+            firstname: event.target.value
+        });
+        
+    };
+    
+    onSubmit = event => {
+        const { firstname, value } = event.target;
+        this.setState({ [firstname]: value })
+        console.log(this.state.firstname);
+        
+            if (event.key === 'Enter' && this.state.firstname !== 'James') {
+                console.log('AAAAAAAAAAAAA');
+                
+                this.setState({
+                    firstnameError: 'Имя указано не верно'
+                });
+            } else {
+                this.renderImage();
+            };   
+    }
+        
+    // добавление сообщения при нажатии Enter
+    sendMessageOnEnter = event => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value })
+      
+        if (event.key === 'Enter' && this.state.firstname !== 'James') {
+            console.log('No Name');
+            
+            this.setState({
+                firstnameError: 'Имя указано не верно'
+            });
+        } else {
+            this.setState({
+                firstnameError: this.state.firstname
+            });
+        }
     };
 
-    // обеспечиваем видимость элементов в окне формы
-    componentDidUpdate() {
-        this.scrollVisible();
-      }    
-  
-      // отслеживание изменений поля ввода
-      changeInputMessage = event => {
-          this.setState({
-              messageInput: event.target.value
-          });
-      };
-  
-      // добавление сообщения при нажатии Enter
-      sendMessageOnEnter = event => {
-        const { messages, messageInput } = this.state;
-        
-          if (event.key === 'Enter') {
-  
-              // добавляем запись в список messages, в конец списка
-              this.setState({
-                messages: [...messages, { text: messageInput }],
-                messageInput: ''
-              });
-  
-          } else {
-              return false;
-          }
-      };
-  
-      scrollVisible() {
-        if (this.elem) {
-          this.elem.scrollIntoView({ block: 'end', behavior: 'smooth' });
-        }
-      }
-  
-      render() {
+    renderImage = () => {
+        return <img src={Bond} alt="bond approve" className="t-bond-image" />;
+    };
+
+    renderForm() {
+        console.log(this.state.firstnameError);
           return (
-              <div className="chat">
-                  <div className="message-list">
-                      <div className="messages"
-                        ref={elem => {
-                          this.elem = elem;
-                        }}
-                      >
-                          {this.state.messages.map((message, index) => {
-                              return <Message key={index} text={message.text}/>;
-                          })}
-                      </div>
-                  </div>
-  
-                  <input
-                      className="input-message"
-                      value={this.state.messageInput}
-                      onChange={this.changeInputMessage}
-                      onKeyPress={this.sendMessageOnEnter}
-                  />
-              </div>
-  
-          );
-      }
-  }    
+            <form className="form" onSubmit={this.onSubmit}>
+                <h1>Введите свои данные, агент</h1>
+
+                <p className="field">
+                    <label className="field__label" htmlFor="firstname">
+                        <span className="field-label">Имя</span>
+                    </label>
+                    <input
+                        className="field__input field-input t-input-firstname"
+                        type="text"
+                        name="firstname"
+                        value={this.state.firstname}
+                        onChange={this.changeInputMessage}
+                        onKeyPress={this.handleUserInput}
+                    />
+                    <span className="field__error field-error t-error-firstname">
+                        {this.state.firstname} 
+                    </span>
+                </p>    
+                <div className={'form__buttons'}>
+                    <button className={'button t-submit'} onClick={this.handleUserInput}>Проверить</button>
+                </div>
+            </form>
+        );
+    };
+
+    render() {
+        return (
+            <div className={'app-container'}>
+                {!this.state.isValid ? this.renderForm() : this.renderImage()}
+            </div>
+        );
+    };
+} 
