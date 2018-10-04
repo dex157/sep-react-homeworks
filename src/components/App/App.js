d2dc516021a5270e9c6fd05e50bddae352150d86
astepanov <stepanov.alexey@gtrf.ru>
2018-10-04 16:41:36 +0300

    init commit for transfer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Todo from '../Todo';
+import { load, save } from '../../localstorage';
 
 class App extends PureComponent {
   render() {
     return (
       <main className="main">
         <div className="main__cell">
-          <Todo />
+          <Todo className='todo-item' saveData={save} savedData={load('todo-item')}/>
         </div>
       </main>
     );
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -14,25 +14,81 @@ class Todo extends PureComponent {
     return biggest + 1;
   }
 
-  handleChange = event => {};
+  handleChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
 
-  createNewRecordByEnter = event => {};
+  createNewRecordByEnter = event => {
+    const { inputValue } = this.state;
+
+    if (event.key === 'Enter' && inputValue) {
+      this.createNewRecord(inputValue);
+    }
+  };
 
   toggleRecordComplete = event => {};
 
-  createNewRecord = () => {};
+  createNewRecord = inputValue => {
+    let { saveData, savedData } = this.props;
+    if (inputValue) {
+      var newRecord = { id: this.getId(), text: inputValue, isComplete: false };
+      var data = [...savedData, newRecord];
+
+      saveData('todo-item', data);
+      
+    }
+  };
 
   render() {
-    return;
+    const data = this.props.savedData;
+    return (
+      <Card title="Список дел">
+        <div className="todo t-todo-list">
+          {this.renderEmptyRecord()}
+          {data
+            ? data.map(record => {
+                return this.renderRecord(record);
+              })
+            : null}
+        </div>
+      </Card>
+    );
   }
 
   renderEmptyRecord() {
-    return;
+    const { inputValue } = this.state;
+    return (
+      <div className="todo-item todo-item-new">
+        <input
+          className="todo-input t-input"
+          placeholder="Введите задачу"
+          value={inputValue}
+          onChange={this.handleChange}
+          onKeyPress={this.createNewRecordByEnter}
+        />
+        <span
+          className="plus t-plus"
+          onClick={this.createNewRecord(inputValue)}
+        >
+          +
+        </span>
+      </div>
+    );
   }
 
   renderRecord = record => {
-    return;
+    return (
+      <div className="todo-item t-todo" key={record.id}>
+        <p className="todo-item__text">{record.text}</p>
+        <span
+          className="todo-item__flag t-todo-complete-flag"
+          data-todo-id={record.id}
+        >
+          {record.isComplete ? '[ x ]' : '[ ]'}
+        </span>
+      </div>
+    );
   };
 }
 
-export default withLocalstorage('todo-app', [])(Todo);
+export default Todo;
