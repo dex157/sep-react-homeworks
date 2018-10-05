7315504c0fa642352395b1bfb175134c46138eed
Ivan Opanasiuk <vanjoker88@gmail.com>
2018-10-05 14:35:06 +0300

    added form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,109 +1,130 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import './Form.css';
-import BondProfile from './assets/bond_approve.jpg';
+import isLoginPic from './assets/bond_approve.jpg';
 
-class Form extends Component {
+class Form extends React.Component {
   state = {
     inputs: {
       firstname: {
         inputName: 'Имя',
         inputValue: '',
-        errors: {
-          emptyMessage: 'Нужно указать имя',
-          wrongMessage: 'Имя указан не верно'
-        }
+        error: ''
       },
       lastname: {
         inputName: 'Фамилия',
         inputValue: '',
-        errors: {
-          emptyMessage: 'Нужно указать фамилию',
-          wrongMessage: 'Фамилия указан не верно'
-        }
+        error: ''
       },
       password: {
         inputName: 'Пароль',
         inputValue: '',
-        errors: {
-          emptyMessage: 'Нужно указать пароль',
-          wrongMessage: 'Пароль указан не верно'
-        }
+        error: ''
       }
     },
-    isAdmin: {
+    admin: {
       firstname: 'james',
       lastname: 'bond',
       password: '007'
     },
-    errors: {
-      firstname: '',
-      lastname: '',
-      password: ''
-    },
-    isLogin: false
+    isLogIn: false
   };
 
-  handleKeyPress = (e) => {
-    if(e.key === "Enter" || e.keyCod === 13) {
-      console.log('handleKeyPress')
-    };
-  }
-  handleOnSubmitForm = (e) => {
-    console.log('handleOnSubmitForm')
-  }
-  handleChange = (e) => {
-    console.log('handleChange');
-  }
-
-
-
-
+  handleOnSubmit = e => {
+    if (e && e.type === 'submit') {
+      e.preventDefault();
+    }
+    const { inputs, admin } = this.state;
+    Object.keys(inputs).map(field => {
+      let value = inputs[field].inputValue;
+      if (value) {
+        if (value.toLowerCase() === admin[field]) {
+          console.log(inputs[field].inputValue, 'true');
+          return '';
+        } else {
+          this.setState({
+            inputs: {
+              ...this.state.inputs,
+              [field]: {
+                ...inputs[field],
+                error: `неверно указано ${inputs[field].inputName}`
+              }
+            }
+          });
+          console.log(inputs[field].inputValue, 'false');
+          return '';
+        }
+      } else {
+          console.log(inputs[field].inputName);
+        this.setState({
+          inputs: {
+            ...this.state.inputs,
+            [field]: {
+              ...this.state.inputs[field],
+              error: `необходимо указать ${inputs[field].inputName}`
+            }
+          }
+        });
+        return false;
+      }
+    });
+  };
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.handleOnSubmit();
+    }
+  };
+  handleOnChange = e => {
+    this.setState({
+      inputs: {
+        ...this.state.inputs,
+        [e.target.name]: {
+          ...this.state.inputs[e.target.name],
+          inputValue: e.target.value
+        }
+      }
+    });
+  };
 
   render() {
-    const { inputs, errors, isLogin } = this.state;
-
+    const { isLogIn, inputs } = this.state;
     return (
       <div className="app-container">
-        {isLogin ? (
-          <img src={BondProfile} className="t-bond-image" alt="картинки нет" />
+        {isLogIn ? (
+          <img src={isLoginPic} alt="bond approve" className="t-bond-image" />
         ) : (
-          <div>
-            <form
-              className="form"
-              onKeyPress={this.handleKeyPress}
-              onSubmit={this.handleOnSubmitForm}
-            >
-              <h1>Введите свои данные, агент</h1>
-              {Object.keys(this.state.inputs).map((fieldname, key) => (
-                <div className="field" key={key}>
-                  <label className="field__label" htmlFor={fieldname}>
-                    <span className="field-label">
-                      {inputs[fieldname].inputName}
-                    </span>
-                  </label>
-                  <input
-                    className={`t-input-${fieldname}`}
-                    type="text"
-                    name={fieldname}
-                    value={inputs[fieldname].inputValue}
-                    onChange={this.handleChange}
-                  />
-                  <span className={`t-error-${fieldname}`}>
-                    {errors[fieldname]}
-                  </span>
-                </div>
-              ))}
-              <div className="form__buttons">
+          <form
+            className="form"
+            onSubmit={this.handleOnSubmit}
+            onKeyPress={this.handleKeyPress}
+          >
+            <h1>Введите свои данные, агент</h1>
+
+            {Object.keys(inputs).map((field, i) => (
+              <p className="field" key={i}>
+                <label className="field__label" htmlFor={field}>
+                  <span className="field-label">{inputs[field].inputName}</span>
+                </label>
                 <input
-                  type="submit"
-                  className="button t-submit"
-                  value="Проверить"
-                  onClick={this.handleOnSubmitForm}
+                  className={`field__input t-input-${field}`}
+                  type="text"
+                  name={field}
+                  value={inputs[field].inputValue}
+                  onChange={this.handleOnChange}
                 />
-              </div>
-            </form>
-          </div>
+                <span className={`field__error field-error t-error-${field}`}>
+                  {inputs[field].error}
+                </span>
+              </p>
+            ))}
+
+            <div className="form__buttons">
+              <input
+                type="submit"
+                className="button t-submit"
+                value="Проверить"
+              />
+            </div>
+          </form>
         )}
       </div>
     );
