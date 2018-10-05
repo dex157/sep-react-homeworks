fa3d0fce6c8e56184a457ab7a2ea2a766d98a3cc
aa-nikitin <a.nikitin91@mail.ru>
2018-10-05 13:29:25 +0300

    dz3* part-2

---
+++ src/components/Show/Show.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { getShowInfo } from '../../api.js';
+import './Show.css';
+
+class Show extends Component {
+    state = {
+        showId: '',
+        data: ''
+    };
+
+    componentDidUpdate(prevProps) {
+        const { showId } = this.props;
+        if (prevProps.showId !== showId) {
+            getShowInfo(showId).then(data => {
+                this.setState({ showId: showId, data: data });
+            });
+        }
+    }
+    render() {
+        const { data } = this.state;
+        if (data) {
+            return (
+                <div className="show">
+                    <img
+                        className="show-image"
+                        src={data.image.medium}
+                        alt={data.name}
+                    />
+                    <h2 className="show-label t-show-name">{data.name}</h2>
+                    <p className="show-text t-show-genre">
+                        <b>Жанр: </b>
+                        {data.genres.join(', ')}
+                    </p>
+                    <p
+                        className="show-text t-show-summary"
+                        dangerouslySetInnerHTML={{ __html: data.summary }}
+                    />
+                </div>
+            );
+        } else {
+            return (
+                <p className="show-inforation t-show-info">Шоу не выбрано</p>
+            );
+        }
+    }
+}
+
+export default Show;
