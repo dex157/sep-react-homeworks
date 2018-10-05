c7df30d7ac09479a2335c26e52ad995723dd318c
Sergey Kulakovskiy <kulakovskiy.s.v@gmail.com>
2018-10-05 16:27:02 +0300

    homework-localstorage-hoc done

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (storageName, storageData) => (WrapperComponent) => {
+    return class extends Component {
+        static displayName = 'ForwardRef';
+
+        saveData = (data) => {
+            const dataVal = data;
+            save(storageName, dataVal);
+            this.forceUpdate();
+        }
+
+        loadData = () => {
+            return load(storageName) || storageData;
+        }
+
+        render() {
+            return (
+                <WrapperComponent 
+                    saveData = {this.saveData}
+                    savedData = {this.loadData()}
+                />
+            )
+        }
+    }
+
+};
 
 export default withLocalstorage;
