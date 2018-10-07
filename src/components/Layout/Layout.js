7672111d29bdfdd4d9c801063517c0ca3d6ce620
alan <alekov0709@gmail.com>
2018-10-07 19:07:09 +0300

    something commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,16 +3,40 @@ import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
+
   render() {
-    return 'empty';
+    let {header, footer, children} = this.props;
+    return(
+    <Fragment>
+      {header ? this.renderHeader : null}
+      <main className={`main
+      ${header ? 'main-with-header' : ''}
+      ${footer ? 'main-with-footer' : ''}
+      `}>
+      <SectionTitle className="main__title" children="MAIN"/>
+      {children}
+      </main>
+      {footer ? this.renderFooter(footer) : null}
+    </Fragment>
+    )
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    return (
+      <header className="header">
+      <SectionTitle className="header__title" children="HEADER" />
+      <HeaderChild />
+      </header>
+    )
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return (
+      <footer className="footer">
+      <SectionTitle className="footer__title" children="FOOTER" />
+      <FooterChild />
+      </footer>
+    )
   }
 }
 
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -35,8 +35,9 @@ class LoginForm extends PureComponent {
       values: { email, password }
     } = this.state;
     const { authorize } = this.props;
-
-    authorize(email, password);
+    console.log(authorize);
+    
+    // authorize(email, password);
   };
 
   render() {
