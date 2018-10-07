ebc4f1784d0c02d10112424e37cb67ae6d18cf39
Igor Tkachuk <I.Tka4yk@gmail.com>
2018-10-07 13:22:48 +0300

    Implement homework-context-auth

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,44 @@ import './Layout.css';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+    let {header, footer, children} = this.props,
+        withHeader = header ? 'main--with-header' : '',
+        withFooter = footer ? 'main--with-footer' : '';
+
+    return (
+      <Fragment>
+        {
+          header && (
+        <header className = "header">
+          <SectionTitle className = "header__title">HEADER</SectionTitle>
+          <div className = "header__content">
+            { header && this.renderHeader(header) }
+          </div>
+        </header>
+          )}
+
+        <main className = { `main ${withHeader} ${withFooter}`}>
+          <SectionTitle className = "main__title">MAIN</SectionTitle>
+          {children}
+        </main>
+        
+        {
+          footer && (
+        <footer className = "footer">
+          <SectionTitle className = "header_title">FOOTER</SectionTitle>
+            { footer && this.renderFooter(footer) }
+        </footer>
+          )}
+      </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    return <HeaderChild />;
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return <FooterChild />;
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,54 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state = {
+    isAuthorized: false,
+    authorizeError: '',
+    email: ''
+  }
+
+  defaultUser = {
+    email: 'stu@dent.com',
+    password: '123'
+  }
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    
+    return <Provider value = { this.getProviderValue() }>{children}</Provider>;
+  }
+
+  authorize = (email, password) => {
+    const defaultUser = this.defaultUser;
+ 
+    if ((email === defaultUser.email) && (password === defaultUser.password)) {
+      this.setState({
+        isAuthorized: true,
+        authorizeError: '',
+        email: email
+      });
+    } else {
+      this.setState({authorizeError: 'Email или пароль введён не верно'});
+    }
+  }
+
+  logout = () => {
+    this.setState({
+      isAuthorized: false,
+      authorizeError: '',
+      email: ''
+    });
+  }
+
+  getProviderValue = () => {
+    const {isAuthorized, authorizeError, email} = this.state;
+    return ({
+      isAuthorized,
+      authorizeError,
+      email,
+      authorize: this.authorize,
+      logout: this.logout
+    });
   }
 }
 
