c87b3ec36b6e38cdd2c20fee85fff5fc315347e2
eugene-finn <1287175@gmail.com>
2018-10-07 14:36:20 +0300

    homework_3 problems with picture

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,81 +1,161 @@
-import React, { Component } from 'react'; // эту строку надо писать всегда, чтобы babel понимал что мы используем JSX
+import React, { Component } from 'react';
+import ImageBond from './assets/bond_approve.jpg'
+import './Form.css';
+
+const validParams = {
+    firstname: 'james',
+    lastname: 'bond',
+    password: '007'
+};
+
+const fields = [
+    {
+        label: 'Имя',
+        type: 'text',
+        name: 'firstname',
+        errorEmpty: 'Нужно указать имя',
+        errorValid: 'Имя указано не верно',
+    },
+    {
+        label: 'Фамилия',
+        type: 'text',
+        name: 'lastname',
+        errorEmpty: 'Нужно указать фамилию',
+        errorValid: 'Фамилия указана не верно',
+    },
+    {
+        label: 'Пароль',
+        type: 'password',
+        name: 'password',
+        errorEmpty: 'Нужно указать пароль',
+        errorValid: 'Пароль указан не верно',
+    },
+]
+
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+    // constructor(props) {
+    // super(props);
+    //     this.state = {
 
-    // componentDidCatch(error, info) {
-    //     // Отображение резервного UI
-    //     this.setState({ hasError: true });
-    //     // Вы можете прологировать ошибку с помощью сервиса отчета об ошибках
-    //     // logErrorToMyService(error, info);
+
+    //     };
     // }
 
     state = {
-        login: '',
-        lastName: '',
-        password: ''
+        firstname: '',
+        lastname: '',
+        password: '',
+        errors: {
+            firstname: '',
+            lastname: '',
+            password: '',
+            formValid: false
+        }
+    };
+
+    // Валидатор возвращает пустой текст или текст ошибки
+    validation(fieldName) {
+        const fieldsItem = fields.find(el => el.name === fieldName);
+        if (this.state[fieldName]) {
+            if (this.state[fieldName] === validParams[fieldName]) {
+                return false;
+            } else {
+                return fieldsItem.errorValid;
+            }
+        } else {
+            return fieldsItem.errorEmpty;
+        }
+    }
+
+
+    handleOnSubmit = event => {
+
+        event.preventDefault();
+        let errors = {};
+
+        for (const item of fields) {
+            if (this.validation(item.name)) {
+                errors[item.name] = this.validation(item.name);
+            }
+        }
+        if (Object.keys(errors).length !== 0) {
+            this.setState(state => ({
+                errors: {
+                    ...state.errors,
+                    ...errors
+                }
+            }));
+        } else {
+            this.setState({
+                formValid: true
+            });
+        }
     };
 
     handleOnChange = event => {
-        this.setState({ login: event.target.login });
-        this.setState({ lastName: event.target.lastName });
-        this.setState({ password: event.target.password });
-    };
-
-    handleonSubmit = event => {
-        if (this.login != "James") {
-            throw new Error('<span>Имя указано не верно</span>');
-            throw new Error('<span>Фамилия указана не верно</span>');
-            throw new Error('<span>Нужно указать пароль</span>');
-        }
-    };
+        // Должен быть один, примерно такой
+        this.setState({
+            errors: {
+                firstname: '',
+                lastname: '',
+                password: ''
+            },
+            [event.target.name]: event.target.value
+        })
+    }
 
     render() {
-        if (this.state.hasError) {
-            // Вы можете отрисовать любой резервный UI
-            return <span>Имя указано не верно</span>;
-            return <span>Фамилия указана не верно</span>;
-            return <span>Нужно указать пароль</span>;
-        }
-        // return this.props.children;
-
+        const { formValid } = this.state;
         return (
-            <Form>
-            <div>
-                <p>{this.state.login}</p>
-                <form class="form">
-                    <h1>Введите свои данные, агент</h1>
-                    <p class="field">
-                        <label class="field__label" for="firstname"><span class="field-label">Имя</span></label>
-                        <input onChange={this.handleOnChange} class="field__input field-input t-input-firstname" type="text" name="firstname" value="lkl"></input>
-                            <span class="field__error field-error t-error-firstname">this.state.hasError.login</span>
-                    </p>
-                        <p class="field">
-                            <label class="field__label" for="lastname"><span class="field-label">Фамилия</span></label>
-                            <input onChange={this.handleOnChange} class="field__input field-input t-input-lastname" type="text" name="lastname" value="lklk"></input>
-                            <span class="field__error field-error t-error-lastname">this.state.hasError.lastName</span>
-                        </p>
-                        <p class="field">
-                            <label class="field__label" for="password"><span class="field-label">Пароль</span></label>
-                            <input onChange={this.handleOnChange}class="field__input field-input t-input-password" type="password" name="password" value=""></input>
-                            <span class="field__error field-error t-error-password">this.state.hasError.password</span>
-                        </p>
-                        <div class="form__buttons">
-                            <input type="submit" class="button t-submit" value="Проверить"></input>
-                        
-                        </div>
-            </form>
-
-                    <input login={this.state.login}  />
-                    <input lastName={this.state.lastName}  />
-                    <input password={this.state.password} onChange={this.handleOnChange} />
-                    <button onSubmit={this.handleonSubmit}>Проверить </button>
-                    {/* onSubmit={this.props.onSubmit} */}
+            <div className="app-container">
+                {formValid ? (
+                    <Approve />
+                ) : (
+                        <form className="form">
+                            <h1> Введите свои данные, агент </h1>
+                            {fields.map(fieldName => (
+                                <p className="field" key={fieldName.name}>
+                                    <label className="field__label" htmlFor={fieldName.name}>
+                                        <span className="field-label"> {fieldName.label} </span>
+                                    </label>
+                                    <input
+                                        className={`field__input field-input t-input-${
+                                            fieldName.name
+                                            }`}
+                                        type={fieldName.type}
+                                        name={fieldName.name}
+                                        onChange={this.handleOnChange}
+                                        value={this.state[fieldName.name]}
+                                    />
+                                    <span
+                                        className={`field__error field-error t-error-${
+                                            fieldName.name
+                                            }`}
+                                    >
+                                        {this.state.errors[fieldName.name]}
+                                    </span>
+                                </p>
+                            ))}
+                            <div className="form__buttons">
+                                <input
+                                    onClick={this.handleOnSubmit}
+                                    className="button t-submit"
+                                    type="submit"
+                                    value="Проверить"
+                                />
+                            </div>
+                        </form>
+                    )}
             </div>
-            </Form>
+        );
+    }
+}
+
+class Approve extends Component {
+    render() {
+        return (
+            <img scr={ImageBond} alt="Картинка James Bond" className="t-bond-image" />
         );
     }
 }
