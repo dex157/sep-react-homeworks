9f136a998ae20c95ad5b6e718282ce9c921dcd8d
aa-nikitin <a.nikitin91@mail.ru>
2018-10-08 16:15:37 +0300

    dz-4

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,55 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
-  render() {
-    return 'empty';
-  }
+    render() {
+        const { header, children, footer } = this.props;
 
-  renderHeader(HeaderChild) {
-    return 'empty';
-  }
+        return [
+            this.checkBlock(header, this.renderHeader(header)),
+            this.renderMain(children),
+            this.checkBlock(footer, this.renderFooter(footer))
+        ];
+    }
 
-  renderFooter(FooterChild) {
-    return 'empty';
-  }
+    checkBlock(checkElem, output) {
+        return checkElem ? output : '';
+    }
+
+    renderHeader(HeaderChild) {
+        return (
+            <header key="header" className="header">
+                <SectionTitle className={'header__title'} children="Header" />
+                {HeaderChild && <HeaderChild />}
+            </header>
+        );
+    }
+
+    renderMain(children) {
+        const { header, footer } = this.props;
+        return (
+            <main
+                className={`main ${this.checkBlock(
+                    header,
+                    'main--with-header'
+                )} ${this.checkBlock(footer, 'main--with-footer')}`}
+                key="main"
+            >
+                <SectionTitle className={'main__title'} children="MAIN" />
+                {children}
+            </main>
+        );
+    }
+
+    renderFooter(FooterChild) {
+        return (
+            <footer key="footer" className="footer">
+                <SectionTitle className={'header__title'} children="Footer" />
+                {FooterChild && <FooterChild />}
+            </footer>
+        );
+    }
 }
 
 export default Layout;
