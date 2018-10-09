7656d63d2929d39d11c19396e85da24df0ef2833
Arabei Konstantin <arabey.k@gmail.com>
2018-10-10 02:35:25 +0800

    homework-4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,31 @@
 import React, { PureComponent } from 'react';
 import { AuthConsumer } from '../../contexts/Auth';
-import Button from '../Button'
+import Button from '../Button';
 import './Header.css';
 
 class Header extends PureComponent {
   render() {
-    return ('empty');
+    return (
+      <AuthConsumer>
+        {({ isAuthorized, email, logout }) =>
+          isAuthorized ? (
+            <div className="header__content">
+              <div className="header-menu">
+                <p className="header-menu__email header-email t-header-email">
+                  {email}
+                </p>
+                <Button
+                  onClick={logout}
+                  className="header-menu__button t-logout"
+                >
+                  Выйти
+                </Button>
+              </div>
+            </div>
+          ) : null
+        }
+      </AuthConsumer>
+    );
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,48 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    isAuthorized: false,
+    authorizeError: ''
+  };
+
+  authorize = (email, password) => {
+    if ((email === 'stu@dent.com') && password === '123') {
+      this.setState({ isAuthorized: true, email: email, authorizeError: '' });
+    } else {
+      this.setState({
+        isAuthorized: false,
+        authorizeError: 'Email или пароль введён не верно'
+      });
+    }
+  };
+
+  logout = () => {
+    this.setState({
+      email: '',
+      isAuthorized: false,
+      authorizeError: ''
+    });
+  };
+
+  getProviderValue = () => {
+    const { isAuthorized, authorizeError, email } = this.state;
+
+    return {
+      email,
+      isAuthorized,
+      authorizeError,
+      authorize: this.authorize,
+      logout: this.logout,
+      sendError: this.sendError
+    };
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
