51fbea371875476bc95c477034ef8a3df6b03ba2
Slav <shutrow@rambler.ru>
2018-10-12 15:59:42 +0300

    CDM with show and data

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -8,41 +8,45 @@ function stripHTML(data) {
 
 class Show extends PureComponent {
     state = {
+        showId: '',
         data: null
       };
 
     componentDidMount() {
+        const  { show }  = this.props ;
         
-        const { show } = this.props;
         if (show !== '') {
             getShowInfo(show).then(data =>
-                this.setState({ data,
+                this.setState({ 
+                    showId: show,
+                    data
                 }),
             );
-        }
+        } 
     }
 
     render() {
         
-        const {data} = this.state;
+        const { data,  showId } = this.state;
+        console.log(showId);
+        if(showId !== '') {
         
-        if(data === null) {
-            return <p className="show-inforation t-show-info">Шоу не выбрано</p>
-        }
-        console.log('key - Component Did Mount, 2 поля в state — showId и data');
-        return (
-            <div className="show">
-            
-                <img className="show-image" src={data.image.original} alt={ data.name }></img>
-                <h2 className="show-label t-show-name">{data.name}</h2>
-                <p className="show-text t-show-genre">
-                    <b>Жанр:</b> { data.genres.join(', ') }
-                </p>
-                <p className="show-text t-show-summary" dangerouslySetInnerHTML={stripHTML(data.summary)} />
+            return (
+                <div className="show">
                 
+                    <img className="show-image" src={ data.image.original } alt={ data.name }></img>
+                    <h2 className="show-label t-show-name">{ data.name }</h2>
+                    <p className="show-text t-show-genre">
+                        <b>Жанр:</b> { data.genres.join(', ') }
+                    </p>
+                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={ stripHTML(data.summary) } />
+                    
 
-            </div>
-        );
+                </div>
+            );
+        } else {
+            return <p className="show-inforation t-show-info">Шоу не выбрано</p>
+        } 
     }
 }
 
