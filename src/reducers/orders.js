9ac00570492ba5fa5e44e9ab0d00bd345cc4e595
Ruslan Koroteev <menu85@yandex.ru>
2018-10-12 16:20:31 +0200

    start

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -3,7 +3,24 @@ import { MOVE_ORDER_NEXT, MOVE_ORDER_BACK } from '../actions/moveOrder';
 import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
+  const { payload } = action;
+  let index, currentOrder;
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      return [
+        ...state,
+        {
+          id: payload.id,
+          recipe: payload.recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ];
+
+    case MOVE_ORDER_NEXT:
+      currentOrder = changePosition(state, payload, 'next');
+      index = state.findIndex(order => order.id === payload);
+      return state.map((order, key) => (key === index ? currentOrder : order));
     default:
       return state;
   }
@@ -11,3 +28,9 @@ export default (state = [], action) => {
 
 export const getOrdersFor = (state, position) =>
   state.orders.filter(order => order.position === position);
+
+export function changePosition(state, payload, move) {
+  let currentOrder = state.find(order => order.id === payload);
+
+  return currentOrder;
+}
