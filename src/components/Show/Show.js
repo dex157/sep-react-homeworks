a0fbfeaaefd4438ae627411ddb4dcf37281c9ee4
Slav <shutrow@rambler.ru>
2018-10-12 10:07:34 +0300

    showId added

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -9,36 +9,40 @@ function stripHTML(data) {
 class Show extends PureComponent {
 
     state = {
+        showId: '',
         data: null
       };
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate(prevProps, prevState) {
         const { showId } = this.props;
 
-        if (showId !== '' && showId !== prevProps.showId) {
+        if (prevProps !== prevState) {
             getShowInfo(showId).then(data =>
-                this.setState({ data }),
+                this.setState({ 
+                    showId,
+                    data 
+                }),
             );
         }
     }
 
     render() {
-        console.log('CDU - Component Did Update, 2 поля в state — showId и data');
-        const {data} = this.state;
-
-        if(data === null) {
+        
+        const { data, showId } = this.state;
+console.log(showId);
+        if(showId === '') {
             return <p className="show-inforation t-show-info">Шоу не выбрано</p>
         }
         
         return (
             <div className="show">
             
-                <img className="show-image" src={data.image.original} alt={ data.name }></img>
-                <h2 className="show-label t-show-name">{data.name}</h2>
+                <img className="show-image" src={ data.image.original } alt={ data.name }></img>
+                <h2 className="show-label t-show-name">{ data.name }</h2>
                 <p className="show-text t-show-genre">
                     <b>Жанр:</b> { data.genres.join(', ') }
                 </p>
-                <p className="show-text t-show-summary" dangerouslySetInnerHTML={stripHTML(data.summary)} />
+                <p className="show-text t-show-summary" dangerouslySetInnerHTML={ stripHTML(data.summary) } />
                 
 
             </div>
