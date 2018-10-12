0c7edfc84c6dc708e6622da4769fe8e69992bcf3
Slav <shutrow@rambler.ru>
2018-10-12 11:44:03 +0300

    question about infinite server requests

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -13,10 +13,11 @@ class Show extends PureComponent {
         data: null
       };
 
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProp, prevState) {
         const { showId } = this.props;
+        console.log(prevProp);
 
-        if (prevProps !== prevState) {
+        if (prevProp !== prevState) {
             getShowInfo(showId).then(data =>
                 this.setState({ 
                     showId,
@@ -29,24 +30,25 @@ class Show extends PureComponent {
     render() {
         
         const { data, showId } = this.state;
-console.log(showId);
-        if(showId === '') {
-            return <p className="show-inforation t-show-info">Шоу не выбрано</p>
-        }
-        
-        return (
-            <div className="show">
-            
-                <img className="show-image" src={ data.image.original } alt={ data.name }></img>
-                <h2 className="show-label t-show-name">{ data.name }</h2>
-                <p className="show-text t-show-genre">
-                    <b>Жанр:</b> { data.genres.join(', ') }
-                </p>
-                <p className="show-text t-show-summary" dangerouslySetInnerHTML={ stripHTML(data.summary) } />
-                
 
-            </div>
-        );
+        if(showId !== '') {
+        
+            return (
+                <div className="show">
+                
+                    <img className="show-image" src={ data.image.original } alt={ data.name }></img>
+                    <h2 className="show-label t-show-name">{ data.name }</h2>
+                    <p className="show-text t-show-genre">
+                        <b>Жанр:</b> { data.genres.join(', ') }
+                    </p>
+                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={ stripHTML(data.summary) } />
+                    
+
+                </div>
+            );
+        } else {
+            return <p className="show-inforation t-show-info">Шоу не выбрано</p>
+        }   
     }
 }
 
