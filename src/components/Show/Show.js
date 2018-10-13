5f9fa564ffadac78bd201b7ae1247893a8fc1491
vostack13 <stanislav.vozikov@gmail.com>
2018-10-13 09:24:05 +0300

    add: выполненная домашняя работа №3*-1

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -1,47 +1,52 @@
-import React, { Component, PureComponent } from 'react'
-import './Show.css'
+import React, { Component, PureComponent } from 'react';
+import './Show.css';
 import { getShowInfo } from '../../api';
 
 export default class Show extends PureComponent {
-	state = {
-		show: '',
-		data: null,
-	}
+  state = {
+    showId: '',
+    data: null
+  };
 
-	// Проверяем изменился ли props.showId с прошлого рендеринга
-	static getDerivedStateFromProps(nextProps, prevState) {
-		return nextProps.showId !== prevState.show 
-			? {data: null, show: nextProps.showId}
-			: null
-	}
+  // Проверяем изменился ли props.showId с прошлого рендеринга
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return nextProps.showId !== prevState.showId
+      ? { data: null, showId: nextProps.showId }
+      : null;
+  }
 
-	// После рендера компоненты ждем новых данных для state.data
-	componentDidUpdate() {
-		const {show, data} = this.state
+  // После рендера компоненты ждем новых данных для state.data
+  componentDidUpdate() {
+    const { showId, data } = this.state;
 
-		if (show !== '' && data === null) {
-			getShowInfo(show).then(resp => {
-				this.setState({ data: resp })
-			})
-		}
-	}
-	
-	render() {
-		const { data } = this.state
-		
-		console.log(data)
-		return data == null
-			? (<p>Шоу не выбрано</p>)
-			: (<div className="show">
-				<img className="schow-image" src={data.image.medium} alt="Картинка" />
-				<h2>{data.name}</h2>
-				<p><b>Жанр: </b> 
-					{data.genres.map(item => {
-						return `${item}, `
-					})}
-				</p>
-				<p>Описание</p>
-			</div>
-		)
-	}
+    if (showId !== '' && data === null) {
+      getShowInfo(showId).then(resp => {
+        this.setState({ data: resp });
+      });
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return data ? (
+      <div className="show">
+        <img className="show-image" src={data.image.medium} alt={data.name} />
+        <h2 className="show-label t-show-name">{data.name}</h2>
+        <p className="show-text t-show-genre">
+          <b>Жанр: </b>
+          {data.genres.map(
+            (genre, index, arr) =>
+              index !== arr.length - 1 ? `${genre}, ` : genre
+          )}
+        </p>
+        <p
+          className="show-text t-show-summary"
+          dangerouslySetInnerHTML={{ __html: data.summary }}
+        />
+      </div>
+    ) : (
+      <p className="show-inforation t-show-info">Шоу не выбрано</p>
+    );
+  }
 }
