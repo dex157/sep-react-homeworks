f7eb1ebf6895eb15cdd4fb94070f6415a4be7711
olaeseane <olaesean@gmail.com>
2018-10-13 17:02:19 +0300

    done homework-localstorage-hoc

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (keyStorage, defaultStorage) => WrappedComponent => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        savedData: load(keyStorage) || defaultStorage
+      };
+    }
+
+    saveData = values => {
+      save(keyStorage, values);
+      this.setState({ savedData: load(keyStorage) });
+    };
+
+    render() {
+      const { savedData } = this.state;
+
+      return (
+        <WrappedComponent saveData={this.saveData} savedData={savedData} />
+      );
+    }
+  };
+};
 
 export default withLocalstorage;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -3,6 +3,24 @@ import Card from '../Card';
 import './Todo.css';
 import withLocalstorage from '../../HOCs/withLocalstorage';
 
+const NewTodoItem = props => {
+  const { title, value, onItemChange, onAddBtnClick, onEnterPress } = props;
+  return (
+    <div className="todo-item todo-item-new">
+      <input
+        className="todo-input t-input"
+        placeholder={title}
+        value={value}
+        onChange={onItemChange}
+        onKeyPress={onEnterPress}
+      />
+      <span className="plus t-plus" onClick={onAddBtnClick}>
+        +
+      </span>
+    </div>
+  );
+};
+
 class Todo extends PureComponent {
   state = {
     inputValue: ''
@@ -14,25 +32,85 @@ class Todo extends PureComponent {
     return biggest + 1;
   }
 
-  handleChange = event => {};
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ inputValue: value });
+  };
 
-  createNewRecordByEnter = event => {};
+  createNewRecordByEnter = event => {
+    if (event.key === 'Enter') {
+      this.createNewRecord();
+    }
+  };
 
-  toggleRecordComplete = event => {};
+  toggleRecordComplete = event => {
+    const { todoId } = event.target.dataset;
+    const { savedData, saveData } = this.props;
+    savedData[todoId - 1] = {
+      ...savedData[todoId - 1],
+      isComplete: !savedData[todoId - 1].isComplete
+    };
+    saveData(savedData);
+  };
 
-  createNewRecord = () => {};
+  createNewRecord = () => {
+    const { saveData, savedData } = this.props;
+    const { inputValue } = this.state;
+    const idNewRecord = this.getId();
+    if (inputValue !== '') {
+      saveData([
+        ...savedData,
+        {
+          id: idNewRecord,
+          isComplete: false,
+          data: inputValue
+        }
+      ]);
+      this.setState({ inputValue: '' });
+    }
+  };
 
   render() {
-    return;
+    const { inputValue } = this.state;
+    const { savedData } = this.props;
+
+    return (
+      <Card title="Список дел">
+        <div className="todo t-todo-list">
+          <NewTodoItem
+            title="Введите задачу"
+            value={inputValue}
+            onItemChange={this.handleChange}
+            onAddBtnClick={this.createNewRecord}
+            onEnterPress={this.createNewRecordByEnter}
+          />
+          {savedData.map(value => this.renderRecord(value))}
+        </div>
+      </Card>
+    );
   }
 
+/*
   renderEmptyRecord() {
     return;
   }
+*/
 
   renderRecord = record => {
-    return;
+    const { id, data, isComplete } = record;
+    return (
+      <div key={id} className="todo-item t-todo">
+        <p className="todo-item__text">{data}</p>
+        <span
+          className="todo-item__flag t-todo-complete-flag"
+          data-todo-id={id}
+          onClick={this.toggleRecordComplete}
+        >
+          {isComplete ? '[x]' : '[ ]'}
+        </span>
+      </div>
+    );
   };
 }
 
-export default withLocalstorage('todo-app', [])(Todo);
+export default withLocalstorage('todo-app2', [])(Todo);
