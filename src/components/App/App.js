53558c965ae6220357e09e887a2090a87f1af10d
BelousovDmitrii <dmitriilir86@mail.ru>
2018-10-15 18:43:34 +1000

    fomeworkValidationForm

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Form from '../Form/Form';
+
+export default () => (
+  <div className="app-container">
+    <Form />
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chat from 'components/Chat';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<Chat />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
