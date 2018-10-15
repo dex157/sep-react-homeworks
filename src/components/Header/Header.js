9e9d3217a118e425380d46a138228bb24f83174c
Kirill Basov <bk-172-v@yandex.ru>
2018-10-16 01:15:39 +0400

    hw4 done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,26 @@ import './Header.css';
 
 class Header extends PureComponent {
   render() {
-    return ('empty');
+    return (
+      <AuthConsumer>
+        {({ isAuthorized, email, logout }) =>
+          isAuthorized ? (
+            <div className='header-menu'>
+              <p className='header-menu__email header-email t-header-email'>
+                {email}
+              </p>
+              <Button 
+                onClick={logout} 
+                className='header-menu__button t-logout button'
+                children='Выйти'
+              />
+            </div>
+          ) : (
+            null
+          )
+        }
+      </AuthConsumer>
+    );
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -2,10 +2,62 @@ import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
+const correctLogin = {
+  email: 'stu@dent.com',
+  password: '123'
+};
+
+const initialState = {
+  email: '',
+  isAuthorized: false,
+  authorizeError: ''
+};
+
 class AuthProvider extends PureComponent {
+  state = {
+    ...initialState
+  }
+
+  authorize = (email , password) => {
+    if ((email === correctLogin.email) && (password === correctLogin.password)) {
+      this.setState({
+        email,
+        isAuthorized: true,
+        authorizeError: ''
+      });
+    } else {
+      this.setState({
+        authorizeError: 'Email или пароль введён не верно'
+      });
+    }
+  }
+
+  logout = () => {
+    this.setState({
+      ...initialState
+    });
+  }
+
+  getProviderValue = () => {
+    const { email, isAuthorized, authorizeError } = this.state;
+
+    return {
+      email,
+      isAuthorized,
+      authorizeError,
+      authorize: this.authorize,
+      logout: this.logout
+    };
+  }
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    
+    return (
+      <Provider value={this.getProviderValue()}>
+        {children}
+      </Provider>
+    );
   }
 }
 
