f75ebca53bcb4ab87538d6db4e2f8372bafe184b
Slav <shutrow@rambler.ru>
2018-10-17 11:25:35 +0300

    HOC completed

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (currentStorage, emptyStorage) => (WrappedComponent) => {
+    
+    return class extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            savedData: load(currentStorage) || emptyStorage
+          };
+        }
+    
+        saveData = values => {
+          save(currentStorage, values);
+          this.setState({ savedData: load(currentStorage) });
+        };
+    
+        render() {
+          const { savedData } = this.state;
+    
+          return (
+            <WrappedComponent saveData={this.saveData} savedData={savedData} />
+          );
+        }
+      };
+};
 
 export default withLocalstorage;
