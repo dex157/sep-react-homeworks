9cfa539251a281034f7d66bb5116a94bc8f8da5b
Andrey Vozchikov <avozchikov@exadel.com>
2018-10-22 11:32:15 +0500

    Homework 10: v1.0

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -22,7 +22,7 @@ const styles = theme => ({
 
 class Login extends PureComponent {
   state = {
-    inputValue: ''
+    inputValue: 'NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo'
   };
   handleChange = event => {
     this.setState({ inputValue: event.target.value });
--- src/modules/RoverPhotos/actions.js
+++ src/modules/RoverPhotos/actions.js
@@ -5,9 +5,11 @@ export const changeSol = createAction('ROVER_PHOTOS/CHANGE_SOL');
 export const fetchPhotosRequest = createAction(
   'ROVER_PHOTOS/FETCH_PHOTOS_REQUEST'
 );
+
 export const fetchPhotosSuccess = createAction(
   'ROVER_PHOTOS/FETCH_PHOTOS_SUCCESS'
 );
+
 export const fetchPhotosFailure = createAction(
   'ROVER_PHOTOS/FETCH_PHOTOS_FAILURE'
 );
