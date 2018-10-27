b6f862dfc45f8d7fa435f56f9fd8e901535c6df5
Artem Kostylev <kostylev95@yandex.ru>
2018-10-27 21:01:16 +0300

    init

--- src/modules/Auth/reducer.js
+++ src/modules/Auth/reducer.js
@@ -1,4 +1,13 @@
 import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
-import {  } from './actions';
+import { addApiKey } from './actions';
 
+const apiKey = handleActions(
+  { [addApiKey]: (_, action) => action.payload },
+  null
+);
+
+export default combineReducers({ apiKey });
+
+export const getIsAuthorized = state => !!state.auth.apiKey;
+export const getApiKey = state => state.auth.apiKey;
