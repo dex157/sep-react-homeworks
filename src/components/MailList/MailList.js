b8b917617fe06a271aa15d944978ade880f4d686
mel161 <mel161@yandex.ru>
2018-10-30 09:59:18 +0500

    [fix] MailList unused vars

--- src/components/MailList/MailList.js
+++ src/components/MailList/MailList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './MailList.module.css';
 import cls from 'classnames';
 
