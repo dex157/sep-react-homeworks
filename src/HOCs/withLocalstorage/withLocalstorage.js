88fcfcb99847034b2b2f8257b22e13f9c8ed083e
mel161 <mel161@yandex.ru>
2018-11-04 01:10:13 +0500

    [FIX] Add forwardRef

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -2,24 +2,33 @@ import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
 const withLocalstorage = (key, value) => WrappedComponent => {
-  return class extends Component {
+  class Wrapper extends Component {
     saveData = data => {
       save(key, data);
       this.forceUpdate();
     };
-    savedData = () => {
+
+    loadData() {
       return load(key) || value;
-    };
+    }
 
     render() {
+      const { forwaredRef, ...rest } = this.props;
+
       return (
         <WrappedComponent
-          savedData={this.savedData()}
+          {...rest}
+          ref={forwaredRef}
+          savedData={this.loadData()}
           saveData={this.saveData}
         />
       );
     }
-  };
+  }
+
+  return React.forwardRef((props, ref) => (
+    <Wrapper {...props} forwaredRef={ref} />
+  ));
 };
 
 export default withLocalstorage;
