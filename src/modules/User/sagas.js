15fcfe6e9807464e3d9c47fa75bcc60db1eec219
Marat Sayfutdinov <saymon.161@gmail.com>
2018-11-05 12:29:33 +0300

    homework-9 done

--- src/modules/User/sagas.js
+++ src/modules/User/sagas.js
@@ -1,14 +1,23 @@
 import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
-
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getApiKey } from '../Auth';
+import { getUserInfo } from './api';
 
 function* fetchUserWatcher() {
-  yield takeLatest(?, fetchUserFlow);
+  yield takeLatest(fetchRequest, fetchUserFlow);
 }
 
 export function* fetchUserFlow(action) {
+  const apiKey = yield select(getApiKey);
 
+  try {
+    const data = yield call(getUserInfo, apiKey, action.payload);
+    yield put(fetchSuccess(data));
+  } catch (error) {
+    yield put(fetchFailure(error));
+  }
 }
 
 export default function*() {
-
+  yield fork(fetchUserWatcher);
 }
