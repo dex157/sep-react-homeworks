3efc0c3d5db12234a27364fcec989dad16a6ce42
Irina <irina5687@gmail.com>
2018-11-07 16:08:42 +0300

    hw-pizza

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -2,8 +2,81 @@ import { CREATE_NEW_ORDER } from '../modules/clients';
 import { MOVE_ORDER_NEXT, MOVE_ORDER_BACK } from '../actions/moveOrder';
 import { ADD_INGREDIENT } from '../actions/ingredients';
 
+const positions = [
+  'clients',
+  'conveyor_1',
+  'conveyor_2',
+  'conveyor_3',
+  'conveyor_4',
+  'finish'
+];
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      const { id, recipe } = action.payload;
+      const order = { id, recipe, ingredients: [], position: 'clients' };
+
+      //console.log('state.orders0  ', state);
+
+      //if (state.orders === undefined) return [...state, order];
+      //else return [...state.orders, order]
+
+      return [...state, order];
+
+    case MOVE_ORDER_NEXT:
+      return state.map(order => {
+        if (order.id === action.payload) {
+          //console.log('order.position0  ', order, order.position);
+          const newPositionNumber = positions.indexOf(order.position) + 1;
+          if (positions[newPositionNumber] === 'finish') {
+            const { recipe, ingredients } = order;
+            for (let ingred of recipe)
+              if (ingredients.includes(ingred) !== true) return order;
+          }
+          
+          //console.log('order.position1  ', order, order.position);
+
+          return { ...order, position: positions[newPositionNumber] };
+        }
+        return order;
+      });
+
+    case MOVE_ORDER_BACK:
+      return state.map(order => {
+        if (order.id === action.payload) {
+          console.log('state.orders1  ', state.orders); 
+          console.log('order.id  ', order.id);
+          let newPositionNumber = positions.indexOf(order.position) - 1;
+          if (newPositionNumber === 0) return order;
+          return { ...order, position: positions[newPositionNumber] };
+        }
+
+        return order;
+      });
+
+    case ADD_INGREDIENT:
+      let notFound = true;
+      const { from, ingredient } = action.payload;
+      
+      //console.log('from, ingredient  ', from, ingredient);
+
+      return state.map(order => {
+        if (order.position === from && notFound) {
+          if (
+            order.recipe.includes(ingredient) === true &&
+            order.ingredients.includes(ingredient) === false
+          ) {
+            notFound = false;
+            return {
+              ...order,
+              ingredients: [...order.ingredients, ingredient]
+            };
+          }
+        }
+        return order;
+      });
+
     default:
       return state;
   }
