d21af6b2cf2fc64fdf8d458dcfa9f8486772a6ce
Igor Tkachuk <I.Tka4yk@gmail.com>
2018-11-07 22:47:42 +0200

    Changed store creation

--- src/store.js
+++ src/store.js
@@ -2,13 +2,23 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import searchMiddleware from './middlewares/searchMiddleware'
 
-const getStore = () => createStore(
+// const getStore = () => createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware( searchMiddleware ),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
+//     )
+// );
+
+// export default getStore;
+
+
+export default initialState =>
+  createStore(
     rootReducer,
+    initialState,
     compose(
-        applyMiddleware( searchMiddleware ),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
+      applyMiddleware(searchMiddleware),
+      window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
-);
-
-export default getStore;
-
+  );
